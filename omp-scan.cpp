23920b5042424d75fcce870f5ba04d71c609e964
juexzz <954742885@qq.com>
2023-04-01 23:22:59 -0400

    Initial submission of question 2.

--- omp-scan.cpp
+++ omp-scan.cpp
@@ -7,19 +7,50 @@
 // use long data type to avoid overflow
 void scan_seq(long* prefix_sum, const long* A, long n) {
   if (n == 0) return;
-  prefix_sum[0] = 0;
+  prefix_sum[0] = A[0];
   for (long i = 1; i < n; i++) {
-    prefix_sum[i] = prefix_sum[i-1] + A[i-1];
+    prefix_sum[i] = prefix_sum[i-1] + A[i];
   }
 }
 
-void scan_omp(long* prefix_sum, const long* A, long n) {
-  int p = omp_get_num_threads();
-  int t = omp_get_thread_num();
+void scan_omp(long* prefix_sum, const long* A, long n, int num_threads) {
+  int p = num_threads; // predefined omp threads number
+  // int t = omp_get_thread_num();
   // Fill out parallel scan: One way to do this is array into p chunks
   // Do a scan in parallel on each chunk, then share/compute the offset
   // through a shared vector and update each chunk by adding the offset
   // in parallel
+  long* offset = (long*) malloc(p * sizeof(long));
+  #pragma omp parallel num_threads(p)
+  {
+    int t = omp_get_thread_num();
+    long start = t * n / p;
+    long end = (t+1) * n / p;
+    // long partialsum = A[start];
+    prefix_sum[start] = A[start];
+    for(long i = start+1; i<end; i++){
+      prefix_sum[i] = prefix_sum[i-1] + A[i];
+    }
+    offset[t] = prefix_sum[end-1];
+  }
+  // get correction in serial
+  for(int j=1; j<p; j++){
+    offset[j] = offset[j] + offset[j-1];
+  }
+  // correct the partial sum in parallel again
+  // pay attention to the mismatch
+  #pragma omp parallel num_threads(p)
+  {
+    int t = omp_get_thread_num();
+    long start = t * n / p;
+    long end = (t+1) * n / p;
+    if(t>0){
+      for(long i=start; i<end; i++){
+        prefix_sum[i] = prefix_sum[i] + offset[t-1];
+      }
+    }
+  }
+  free(offset);
 }
 
 int main() {
@@ -34,13 +65,24 @@ int main() {
   scan_seq(B0, A, N);
   printf("sequential-scan = %fs\n", omp_get_wtime() - tt);
 
-  tt = omp_get_wtime();
-  scan_omp(B1, A, N);
-  printf("parallel-scan   = %fs\n", omp_get_wtime() - tt);
+  // better report for different number of threads
+  int max_num_threads = 16;
+  for (int i=1; i<=16; i = i*2){
+    tt = omp_get_wtime();
+    scan_omp(B1, A, N, i);
+    printf("run with %d threads\t", i);
+    printf("parallel-scan   = %fs\t", omp_get_wtime() - tt);
+    long err = 0;
+    for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+    printf("error = %ld\n", err);
+  }
+  // tt = omp_get_wtime();
+  // scan_omp(B1, A, N, max_num_threads);
+  // printf("parallel-scan   = %fs\n", omp_get_wtime() - tt);
 
-  long err = 0;
-  for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
-  printf("error = %ld\n", err);
+  // long err = 0;
+  // for (long i = 0; i < N; i++) err = std::max(err, std::abs(B0[i] - B1[i]));
+  // printf("error = %ld\n", err);
 
   free(A);
   free(B0);
